cffd39d10324cd1aed9c4ffaee4d6e0df9b4588b
Sleepyhead <sean.mclellan@sixconcepts.com>
2014-04-22 00:17:34 -0400

    Bed.

---
+++ V8SignalRDebugging/ClientScriptEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V8SignalRDebugging
+{
+    http://www.asp.net/signalr/overview/signalr-20/getting-started-with-signalr-20/tutorial-server-broadcast-with-signalr-20
+
+    public class ClientScriptEngine
+    {
+    }
+}
